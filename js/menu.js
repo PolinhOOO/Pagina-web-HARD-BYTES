0d27aac2bef363dbdcaa411e28664a0b693b9474
PolinhOOO <66158549+PolinhOOO@users.noreply.github.com>
2020-05-30 01:52:39 -0600

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let BtnMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+BtnMenu.addEventListener('click',function(){
+   'use strict'; 
+    menu.classList.toggle('mostrar');
+});
